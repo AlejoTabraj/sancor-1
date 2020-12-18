a1ea9b648b62528955e101fdec3df8d9407d0281
alejho4 <alejhotabraj@gmail.com>
2020-12-18 20:15:02 +0000

    nord

--- client/src/components/Beneficios/index.js
+++ client/src/components/Beneficios/index.js
@@ -6,12 +6,12 @@ export const Beneficios = () => {
     <div className="beneficiosContainer">
 		<h2>Beneficios para vos y tu familia</h2>
 		<ul>
-			<li>Consultas médicas al 100% en todas las especialidades.</li>
-			<li>Odontología general al 100%.</li>
-			<li>Maternidad: cobertura para la mamá y el recién nacido. Plan Materno Infantil.</li>
-			<li>Internación clínica y quirúrgica, sin costos ni topes de días, en clínicas y sanatorios<br/>
-			de la red de prestadores.</li>
-			<li>100% de cobertura en medicamentos durante internación y 40% en ambulatorios.</li>
+			<li><p>Consultas médicas al 100% en todas las especialidades.</p></li>
+			<li><p>Odontología general al 100%.</p></li>
+			<li><p>Maternidad: cobertura para la mamá y el recién nacido. Plan Materno Infantil.</p></li>
+			<li><p>Internación clínica y quirúrgica, sin costos ni topes de días, en clínicas y sanatorios<br/>
+			de la red de prestadores.</p></li>
+			<li><p>100% de cobertura en medicamentos durante internación y 40% en ambulatorios.</p></li>
 		</ul>
 		
 	</div>
