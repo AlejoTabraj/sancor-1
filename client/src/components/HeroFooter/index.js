089c0ad4205812c211e62dca317d5c1dff1d3e0e
alejho4 <alejhotabraj@gmail.com>
2020-12-20 10:44:43 +0000

    yy

--- client/src/components/HeroFooter/index.js
+++ client/src/components/HeroFooter/index.js
@@ -4,7 +4,9 @@ import './style.css'
 export const HeroFooter = () => {
     return (
     <div className="heroFooter">
+        <div className='footerWrap'>
 		<h2>¡BASTA DE PAGAR DEMÁS POR TU COBERTURA MÉDICA!</h2>
+        </div>
 	</div>
     )
 }
